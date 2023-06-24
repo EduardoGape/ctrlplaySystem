c728ea5b5b00d0f94aa6b6c6bb1d27b3dee464ff
EduardoGape <git.eduardo.leite@gmail.com>
2023-06-24 10:21:18 -0400

    [+] init

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $admins = Admin::all();
+        return response()->json($admins);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $admin =  Admin::create([
+            'name'=> $request->name,
+            'email'=>$request->emil,
+            'password'=>Hash::make($request->password),
+        ]);
+        return response()->json($admin, 201);
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Admin $admin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Admin $admin)
+    {
+        //
+    }
+}
